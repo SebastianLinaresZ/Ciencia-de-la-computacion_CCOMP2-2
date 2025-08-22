be51448843f83d7efd51dfe78da184b1d0a174ae
ALUMNO-D104 <ALUMNO-D104@UCSP.AULAB>
2025-08-22 08:17:15 -0500

    Pro_HolaMundo

--- Sem_2/Semana_dos/main.cpp
+++ Sem_2/Semana_dos/main.cpp
@@ -4,6 +4,9 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int val1 = 10;
+    int val2 = 20;
+    int result = val1 + val2;
+    cout <<"El resultado es: " << result << endl;
     return 0;
 }
